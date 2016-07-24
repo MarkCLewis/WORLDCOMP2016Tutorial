c37987a52b5d1e7a81a08b792d797dee01041a42
= <mlewis@trinity.edu>
2016-07-23 21:18:35 -0500

    Finished bare bones example.

--- barebonesPlay/app/controllers/Application.scala
+++ barebonesPlay/app/controllers/Application.scala
@@ -4,9 +4,18 @@ import play.mvc.Controller;
 import play.api.mvc.Action
 import play.api.mvc.Results._
 import javax.inject.Inject
+import play.twirl.api.Html
 
 class Application @Inject() extends Controller {
   def index = Action {
-	  Ok("This is the text for the page.")
+	  Ok(views.html.index("Reactive Programming"))
+  }
+  
+  def add(a: Int, b: Int) = Action {
+    Ok(views.html.main("Adding")(Html(s"$a + $b = ${a+b}")))
+  }
+  
+  def factorial(n: Int) = Action {
+    Ok(views.html.index(s"$n! = ${(BigInt(1) to BigInt(n)).product}"))
   }
 }
