5c52cf63ead1f48636cc207bdcdc8caeafacc99e
= <mlewis@trinity.edu>
2016-07-25 18:47:40 -0500

    Streaming example is good enough.

---
+++ twitter-streamer/app/actors/TwitterStreamer.scala
@@ -0,0 +1,108 @@
+package actors
+
+import akka.actor._
+import play.api._
+import play.api.libs.json._
+import play.api.libs.oauth._
+import play.api.libs.concurrent.Execution.Implicits._
+
+import scala.collection.mutable.ArrayBuffer
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.ws.WSClient
+import play.api.libs.iteratee.Concurrent.Broadcaster
+import play.api.libs.ws.StreamedResponse
+import akka.stream.scaladsl.Sink
+import akka.stream.ActorMaterializer
+import scala.util.Try
+
+/**
+ * This code was taken from 
+ */
+class TwitterStreamer(out: ActorRef) extends Actor {
+  def receive = {
+    case _ =>
+      Logger.info("Received subscription from a client")
+      TwitterStreamer.subscribe(out)
+  }
+  
+  override def preStart():Unit = {
+    Logger.info("Making Twitter Streamer")
+    super.preStart()
+  }
+
+  override def postStop():Unit = {
+    Logger.info("Client unsubscribing from stream")
+    TwitterStreamer.unsubscribe(out)
+  }
+}
+
+object TwitterStreamer {
+
+//  private var broadcastEnumerator: Option[Enumerator[JsObject]] = None
+  private var stream: Option[StreamedResponse] = None
+
+  private val subscribers = new ArrayBuffer[ActorRef]()
+
+  def subscribe(out: ActorRef)(implicit context: ActorContext): Unit = {
+
+    implicit val materializer = ActorMaterializer()
+    // Connect to stream
+    stream.foreach { source =>
+      Logger.info(source.toString())
+      source.body.
+      scan("")((acc, curr) => if (acc.contains("\r\n")) curr.utf8String else acc + curr.utf8String)
+						.filter(_.contains("\r\n"))
+						.runForeach { tweet =>
+              Logger.info(tweet)
+              out ! tweet.trim
+						}
+//      runForeach { tweet =>
+//        Logger.info(tweet.utf8String)
+//        out ! Json.toJson(tweet.utf8String)
+//      }
+    }
+    
+    Logger.info("Adding subscriber")
+    subscribers += out
+  }
+
+  def unsubscribe(subscriber: ActorRef): Unit = {
+      val index = subscribers.indexWhere(_ == subscriber)
+      if (index > 0) {
+        subscribers.remove(index)
+        Logger.info("Unsubscribed client from stream")
+      }
+  }
+
+  def init(implicit ws:WSClient, configuration: Configuration, system: ActorSystem): Unit = {
+
+    credentials(configuration).map { case (consumerKey, requestToken) =>
+
+      val maybeMasterNodeUrl = Option(System.getProperty("masterNodeUrl"))
+      val url = maybeMasterNodeUrl.getOrElse {
+        "https://stream.twitter.com/1.1/statuses/filter.json"
+      }
+
+      ws.url(url)
+        .sign(OAuthCalculator(consumerKey, requestToken))
+        .withQueryString("track" -> "cat").stream().foreach { s => 
+          stream = Some(s) 
+//          implicit val materializer = ActorMaterializer()
+//          stream.foreach(_.body.runForeach(bs => println(bs.decodeString("UTF-8"))))
+      }
+
+    } getOrElse {
+      Logger.error("Twitter credentials are not configured")
+    }
+
+  }
+
+  private def credentials(configuration: Configuration) = for {
+    apiKey <- configuration.getString("twitter.apiKey")
+    apiSecret <- configuration.getString("twitter.apiSecret")
+    token <- configuration.getString("twitter.token")
+    tokenSecret <- configuration.getString("twitter.tokenSecret")
+  } yield (ConsumerKey(apiKey, apiSecret), RequestToken(token, tokenSecret))
+
+
+}
--- twitter-streamer/app/controllers/Application.scala
+++ twitter-streamer/app/controllers/Application.scala
@@ -2,21 +2,37 @@ package controllers
 
 import play.api.mvc.Controller
 import play.api.mvc.Action
+import play.api.mvc.WebSocket
 import javax.inject.Inject
 import play.api.libs.oauth.RequestToken
 import play.api.libs.oauth.ConsumerKey
+import play.api.libs.json.JsValue
+import actors.TwitterStreamer
+import play.api.libs.json._
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.Materializer
+import play.api.libs.streams._
+import akka.actor.Props
+import play.api.libs.ws.WSClient
 
-class Application @Inject() (environment: play.api.Environment, configuration: play.api.Configuration) extends Controller {
-  val credentials = for {
-    apiKey <- configuration.getString("twitter.apiKey")
-    apiSecret <- configuration.getString("twitter.apiSecret")
-    token <- configuration.getString("twitter.token")
-    tokenSecret <- configuration.getString("twitter.tokenSecret")
-  } yield {
-    (ConsumerKey(apiKey, apiSecret), RequestToken(token, tokenSecret))
-  }
+class Application @Inject() (
+    implicit environment: play.api.Environment, 
+    configuration: play.api.Configuration, 
+    system: ActorSystem, 
+    materializer: Materializer, 
+    ws: WSClient) extends Controller {
   
-  def index = Action {
-    Ok("Your ad goes here. Soon there will be Twitter tweets.")
+  TwitterStreamer.init
+  
+  // The following three methods were taken from the chapter 2 example in Reactive Web Applications
+  def index = Action { implicit request =>
+    Ok(views.html.index("Tweets"))
   }
+
+  def tweets = WebSocket.accept[String, String] { request =>
+    println("Accepting WebSocket")
+    ActorFlow.actorRef(out => Props(new TwitterStreamer(out)))
+  }
+
 }
